113ebf9c02dc4553382984766531903e29f7b986
Katya <ekaterina.safia@yandex.ru>
2023-01-09 13:22:54 +0300

    new com

--- 6.js
+++ 6.js
@@ -2,12 +2,12 @@
 
 // Дан массив. Проверить, одинаковые ли элементы в массиве и вывести результат true или false в консоль. Речь идёт не о двух рядом стоящих одинаковых элементах, а обо всех. Проверить, все ли элементы в массиве одинаковые.
 
-
-let arr = [1, 1, 3, 1];
-for (let i = 0; i < arr.length; i = i + 1) {
-    if (arr[i] === 1) {
-        console.log('true')
-    } else {
-        console.log('false')
+let check = true
+let arr = [1, 1, 1, 1];
+for (let i = 1; i < arr.length; i++) {
+    if (arr[0] != arr[i]) {
+        check = false
+        break
     }
 };
+console.log(check)
